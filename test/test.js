56f92b34ee73a3abf4e633e185eb10001bb0c8ee
Pranjal_pandey98 <pranjal.pandey@nagarro.com>
2022-09-15 17:36:41 +0530

    commit

--- test/test.js
+++ test/test.js
@@ -3,10 +3,6 @@ const app = require("../app");
 
 describe("Test the root path", () => {
   test("It should response the GET method", () => {
-    return request(app)
-      .get("/")
-      .then((response) => {
-        expect(response.statusCode).toBe(200);
-      });
+    return request(app).get("/").expect(200);
   });
 });
