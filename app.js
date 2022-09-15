05831c7a7c1be1b5d5e3c1d467217154fc5dbd8e
Pranjal_pandey98 <pranjal.pandey@nagarro.com>
2022-09-15 18:17:16 +0530

    finally test done

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
 
-app
-  .get("/", (req, res) => {
-    res.status(200).send("Hello World!");
-    console.log("hello world");
-  })
-  .listen(5000);
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+  console.log("hello world");
+});
 
 module.exports = app;
