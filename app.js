a45cf95db08a9e7db43ade0d338c47687bc4b87c
Pranjal_pandey98 <pranjal.pandey@nagarro.com>
2022-09-23 11:01:06 +0530

    deploy

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+  res
+    .status(200)
+    .send(
+      "Hello World!, A Hello, World! program is generally a computer program that ignores any input and outputs or displays a message similar to 'Hello, World!'. A small piece of code in most general-purpose programming languages, this program is used to illustrate a language's basic syntax"
+    );
   console.log("hello world");
 });
 
